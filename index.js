6ddb80f68ec0a5ce04d3a040c75e01c047b8012c
ingalls <nick@mapbox.com>
2017-07-05 17:02:26 +0000

    Rewrite index to expect JSON or JS

--- index.js
+++ index.js
@@ -12,32 +12,47 @@ const path = require('path');
  * @return {Array|Object} Return an array for a single lang tokens or an object map of all tokens by ISO code
  */
 function config(lang, singletons) {
-    'use strict';
     singletons = !!singletons;
 
     if (lang && (typeof lang !== 'string' || lang.length != 2)) throw Error('optional lang param must be string containing 2 letter ISO 639-1 Code');
 
     if (lang) {
-        if (!fs.statSync(path.resolve(__dirname, './tokens/'))) return [];
+        if (!fs.statSync(path.resolve(__dirname, `./tokens/${lang}.json`))) {
+            if (!fs.statSync(path.resolve(__dirname, `./tokens/${lang}.json`))) {
+                return [];
+            } else {
+                let tokenjs = require(`./tokens/${lang}`);
 
-        return singletons ? require(`./tokens/${lang}.json`) : removeSingletons(require(`./tokens/${lang}.json`));
+                return singletons ? tokenjs() : removeSingletons(tokenjs());
+            }
+        } else {
+            let tokenjson = require(`./tokens/${lang}.json`);
+
+            return singletons ? tokenjson : removeSingletons(tokenjson);
+        }
     }
 
     const tokens = {};
 
     fs.readdirSync(path.resolve(__dirname, './tokens/')).forEach((token) => {
-        if (!token.match(/\.json$/)) return;
+        if (token.match(/\.json$/)) {
+            let json = require(`./tokens/${token}`);
 
-        let json = require(`./tokens/${token}`);
+            tokens[token.replace(/\.json/, '')] = singletons ? json : removeSingletons(json);
+        } else if (token.match(/\.js$/)) {
+            let js = require(`./tokens/${token.replace('\.js$', '')}`);
 
-        tokens[token.replace(/\.json/, '')] = singletons ? json : removeSingletons(json);
+            tokens[token.replace(/\.js/, '')] = singletons ? js : removeSingletons(js);
+        } else {
+            return;
+        } 
     });
 
     return tokens;
 }
 
 function removeSingletons(tokens) {
-    if (!(tokens instanceof Array))
-        return tokens;
+    if (!(tokens instanceof Array)) return tokens;
+
     return tokens.filter((token) => { return token instanceof Array && token.length > 1; });
 }
