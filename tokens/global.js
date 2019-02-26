547b4045fea71404b9706ed71aa62e99f432dccb
Andrew Pendleton <andrew@mapbox.com>
2019-02-26 16:36:27 -0500

    Fix Swedish replacements, and move the last English and German global replacements into language-specific, making it possible to simplify the global file.

--- tokens/global.js
+++ tokens/global.js
@@ -1,20 +1,5 @@
 module.exports = () => {
-    let tokens = {};
-
-    //Prepend Standard word boundaries + ^
-    //Postpend Standard word boundaries + $
-    let stdBoundaries = {
-        "P\\.?\\ ?O\\.? Box [0-9]+": " ",
-        "(.+)(strasse|str|straße)": " $1 str "
-    };
-
-    const BOUNDARIES = "[\\s\\u2000-\\u206F\\u2E00-\\u2E7F\\\\'!\"#$%&()*+,\\-.\\/:;<=>?@\\[\\]^_`{|}~]"
-
-    for (let find in stdBoundaries) {
-        tokens[`(?:${BOUNDARIES}|^)${find}(?:${BOUNDARIES}|$)`] = stdBoundaries[find];
-    }
-
-    let custBoundaries = {
+    return {
         "\\b1丁目\\b": " 一丁目 ",
         "\\b2丁目\\b": " 二丁目 ",
         "\\b3丁目\\b": " 三丁目 ",
@@ -26,11 +11,5 @@ module.exports = () => {
         "\\b9丁目\\b": " 九丁目 ",
         "\\b10丁目\\b": " 十丁目 ",
         "^([\\u1100-\\u11FF\\u2E80-\\u2EFF\\u3000-\\u303F\\u3040-\\u309F\\u30A0-\\u30FF\\u3100-\\u312F\\u3130-\\u318F\\u31C0-\\u31EF\\u31F0-\\u31FF\\u3200-\\u32FF\\u3300-\\u33FF\\u3400-\\u4DBF\\u4E00-\\u9FFF\\uF900-\\uFAFF\\uFE30-\\uFE4F\\s]+)(\\s*)([0-9]+)-([0-9]+)(.*)$": "$1$5 $3"
-    }
-
-    for (let find in custBoundaries) {
-        tokens[find] = custBoundaries[find];
-    }
-
-    return tokens;
+    };
 }
