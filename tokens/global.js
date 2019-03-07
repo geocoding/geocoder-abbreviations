f052161dc8ebf08da14fd8afab1719d5e640e295
Andrew Pendleton <andrew@mapbox.com>
2019-03-07 22:48:06 +0000

    Rewrite the global Japanese thing to allow for numbers to be mixed into the Japanese chunk at the beginning, without accidentally consuming the numbers we want to keep by accident. Ugh.

--- tokens/global.js
+++ tokens/global.js
@@ -1,5 +1,15 @@
 module.exports = () => {
-    return {
-        "^([\\u1100-\\u11FF\\u2E80-\\u2EFF\\u3000-\\u303F\\u3040-\\u309F\\u30A0-\\u30FF\\u3100-\\u312F\\u3130-\\u318F\\u31C0-\\u31EF\\u31F0-\\u31FF\\u3200-\\u32FF\\u3300-\\u33FF\\u3400-\\u4DBF\\u4E00-\\u9FFF\\uF900-\\uFAFF\\uFE30-\\uFE4F\\s]+)(\\s*)([0-9]+)-([0-9]+)(.*)$": "$1$5 $3"
-    };
+    const out = {};
+
+    // this is all the characters we expect in Japanese text
+    const JP = '\\u1100-\\u11FF\\u2E80-\\u2EFF\\u3000-\\u318F\\u31C0-\\u4DBF\\u4E00-\\u9FFF\\uF900-\\uFAFF\\uFE30-\\uFE4F';
+    // this matches a string that can either start with a single Japanese character, or a thing that starts and ends
+    // with a Japanese character and has zero or more Japanese characters, numbers of either Latin or Japanese flavor,
+    // or whitespace in the middle. That whole mess is then followed optionally by space, and then number hyphen number,
+    // followed by whatever. We transform it by keeping the whole Japanese chunk, dropping the second number, and moving
+    // the first number to the end (effectively: deletes the building number, and moves the block number into the building-
+    // number position)
+    out[`^([${JP}](?:[${JP}0-9\\uFF10-\\uFF19\\s]*[${JP}])?)(?:\\s*)([0-9]+)-(?:[0-9]+)(.*)$`] = "$1$3 $2";
+
+    return out;
 }
