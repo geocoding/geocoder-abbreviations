d7398349d3532a74aeaacd5606b4786b913e0f8c
Lizzie Gooding <lizzie.gooding@mapbox.com>
2019-04-01 13:28:52 -0400

    match to multiple country token files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use serde::{Deserialize};
-use serde_json::Result;
+use serde::Deserialize;
+use serde_json;
+
+#[derive(Debug)]
+enum CC {
+    De,
+    En,
+    Es,
+    Fi
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    CountryCodeNotSupported
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Token {
@@ -13,12 +26,27 @@ pub struct Token {
     token_type:  Option<String>,
 }
 
-fn main() -> Result<()> {
-    get_tokens()?;
+fn main() -> Result<(), Error> {
+    let tokens = match_tokens(CC::De).unwrap();
+    let parsed_tokens : Vec<Token> = serde_json::from_str(&tokens);
+    println!("{:#?}", parsed_tokens[0]);
     Ok(())
 }
 
-fn get_tokens() -> Result<Vec<Token>> {
-    let my_str = include_str!("../tokens/de.json");
-    serde_json::from_str(my_str)
+fn match_tokens(cc: CC) -> Result<String, Error> {
+    match cc {
+        CC::De => {
+            Ok(include_str!("../tokens/de.json").to_string())
+        }
+        CC::En => {
+            Ok(include_str!("../tokens/en.json").to_string())
+        },
+        CC::Es => {
+            Ok(include_str!("../tokens/es.json").to_string())
+        },
+        CC::Fi => {
+            Ok(include_str!("../tokens/fi.json").to_string())
+        },
+        _ => Err(Error::CountryCodeNotSupported)
+    }
 }
